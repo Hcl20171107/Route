fce74acb83201885dc7f1153e5e761741d59f467
Hcl20171107 <43674629+Hcl20171107@users.noreply.github.com>
2019-06-20 16:37:39 +0800

    获取一张照片的信息
    可获取图片高度、图片宽、拍摄时间、拍摄地点的经度和纬度。共5项。

--- Route/src/com/picture/AccessGPS/Metadate.java
+++ Route/src/com/picture/AccessGPS/Metadate.java
@@ -1,37 +1,40 @@
-package com.picture.AccessGPS;
+package com.picture.AccessGPS;  
+import java.io.File;    
 
-import java.io.File;
-
-import com.picture.AccessGPS.ImageProcessingException;
-
-
-public class Information {
-
+import com.drew.imaging.ImageMetadataReader;  
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;  
+/**
+ * java读取照片信息
+ */
+public class Metadate{
 	public static void main(String[] args) throws Exception, Exception{  
-        File file = new File("C:\\Users\\hp\\Desktop\\张阳阳.jpg"); 
+        File file = new File("C:\\Users\\hp\\Desktop\\张阳阳.jpg");  
         printImageTags(file);
     } 
     /** 
      * 读取照片里面的信息 
      */ 
     private static void printImageTags(File file) throws ImageProcessingException, Exception{  
-        Metadata metadata = ImageMetadataReader.readMetadata(file);  
+    	Metadata metadata = ImageMetadataReader.readMetadata(file);  
         for (Directory directory : metadata.getDirectories()) {  
             for (Tag tag : directory.getTags()) {  
                 String tagName = tag.getTagName();  //标签名
                 String desc = tag.getDescription(); //标签信息
                 if (tagName.equals("Image Height")) {  
-                    System.out.println("图片高度: "+desc);
+                	System.out.println("图片高度: "+desc);
                 } else if (tagName.equals("Image Width")) {  
-                    System.out.println("图片宽度: "+desc);
+                	System.out.println("图片宽度: "+desc);
                 } else if (tagName.equals("Date/Time Original")) {  
-                    System.out.println("拍摄时间: "+desc);
+                	System.out.println("拍摄时间: "+desc);
                 }else if (tagName.equals("GPS Latitude")) {  
-                    System.err.println("纬度 : "+desc);
-//                    System.err.println("纬度(度分秒格式) : "+pointToLatlong(desc));
+                	System.err.println("纬度 : "+desc);
+//                	System.err.println("纬度(度分秒格式) : "+pointToLatlong(desc));
                 } else if (tagName.equals("GPS Longitude")) {  
-                    System.err.println("经度: "+desc);
-//                    System.err.println("经度(度分秒格式): "+pointToLatlong(desc));
+                	System.err.println("经度: "+desc);
+//                	System.err.println("经度(度分秒格式): "+pointToLatlong(desc));
                 }
             }  
         }  
@@ -49,3 +52,4 @@ public class Information {
         return duStr.toString();  
     }  
 }
+ 
